3826ffbb889b1db1ecd32458eea61ef23d2e0d9b
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-03 18:43:04 -0300

    Algumas opcionais

--- query3.sql
+++ query3.sql
@@ -38,3 +38,24 @@ LEFT JOIN pedido ON pedido.`codFornecedor` = fornecedor.`codFornecedor`;
 SELECT departamento.nome, departamento.localizacao, vendedor.nome, vendedor.`dataNascimento`
 FROM vendedor
 RIGHT JOIN departamento ON vendedor.`codDepartamento` = departamento.`codDepartamento`
+
+SELECT classe.nome, produto.descricao, produto.`precoVenda`
+FROM classe
+LEFT JOIN produto ON produto.`codClasse` = classe.`codClasse`;
+
+
+SELECT estado.nome, cidade.nome
+FROM cidade
+RIGHT JOIN estado ON cidade.`siglaEstado` = estado.`siglaEstado`;
+
+SELECT produto.`codProduto`, produto.descricao, `produtoLote`.`numeroLote`, `produtoLote`.`dataValidade`
+FROM produto
+LEFT JOIN `produtoLote` ON produto.`codProduto` = `produtoLote`.`codProduto`;
+
+SELECT cidade.`codCidade`, cidade.nome, cidade.`siglaEstado`, vendedor.nome, vendedor.endereco, vendedor.telefone
+FROM vendedor
+RIGHT JOIN cidade ON vendedor.`codCidade` = cidade.`codCidade`;
+
+SELECT vendedor.nome, vendedor.`dataContratacao`, venda.`codVenda`, venda.`dataVenda`
+FROM vendedor
+LEFT JOIN venda ON venda.`codVendedor` = vendedor.`codVendedor`;
